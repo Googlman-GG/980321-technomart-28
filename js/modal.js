b8902cdd89f9fe0fa39d3ac5affd4a48572ab5bb
Googlman-GG <62950937+Googlman-GG@users.noreply.github.com>
2020-07-12 12:31:12 +0200

    1.11

---
+++ js/modal.js
@@ -0,0 +1,69 @@
+var loginLink = document.querySelector(".modal-link");
+var mapPopup = document.querySelector(".modal-map");
+var mapLink = document.querySelector(".map-btn");
+var loginPopup = document.querySelector(".popup-order");
+var loginClose = loginPopup.querySelector(".popup-close-btn");
+var mapClose = mapPopup.querySelector(".map-close-btn");
+var loginForm = loginPopup.querySelector(".popup-form");
+var loginLogin = loginPopup.querySelector(".name-user");
+var loginPassword = loginPopup.querySelector(".email-user");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+loginLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.add("modal-show");
+
+  if (storage) {
+    loginLogin.value = storage;
+    loginPassword.focus();
+  } else {
+    loginLogin.focus();
+  }
+});
+
+loginClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  loginPopup.classList.remove("modal-show");
+  loginPopup.classList.remove("modal-error");
+});
+
+loginForm.addEventListener("submit", function (evt) {
+  if (!loginLogin.value || !loginPassword.value) {
+    evt.preventDefault();
+    loginPopup.classList.remove("modal-error");
+    loginPopup.offsetWidth = loginPopup.offsetWidth;
+    loginPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("login", loginLogin.value);
+    }
+  }
+});
+
+mapLink.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (loginPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      loginPopup.classList.remove("modal-show");
+      loginPopup.classList.remove("modal-error");
+    }
+  }
+});
